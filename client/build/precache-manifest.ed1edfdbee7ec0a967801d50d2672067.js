b16ea78c0282abf19295d8e459cb2041d5b605ae
Jim Hopkinson <jahopki@hotmail.com>
2020-10-13 20:37:20 -0600

    test

---
+++ client/build/precache-manifest.ed1edfdbee7ec0a967801d50d2672067.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7792696a5f70fa392c4311c014a603fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c5044b579b2b1fdb2fe",
+    "url": "/static/css/main.49b21ed8.chunk.css"
+  },
+  {
+    "revision": "16f4b1e124ecda48c1fa",
+    "url": "/static/js/2.8db5df9a.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.8db5df9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c5044b579b2b1fdb2fe",
+    "url": "/static/js/main.370ca925.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
